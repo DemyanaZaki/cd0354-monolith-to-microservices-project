e2d342eb991f45bc26dde8a7fa1c8fb6b4481913
DemyanaZaki <Damiana21079@feng.bu.edu.eg>
2022-10-09 22:48:59 +0200

    micro services

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=DATABASE
+set POSTGRES_HOST=cdr-database-1.ca0iuzqakoli.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=test-6517-0603-1736-dev1
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
